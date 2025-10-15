4e308bbc3f15f8469d0e2494e590d4fab03ea952
melc <melas.christos@gmail.com>
2025-10-15 17:27:22 +0300

    - setup node.js project - add config file and env.sample - setup orm

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+export const APP_PORT = parseInt(process.env.PORT || '', 10);
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PORT = parseInt(process.env.DB_PORT || '', 10);
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME;
+export const DB_DIALECT = process.env.DB_DIALECT;
+export const SOCIAL_MEDIA_API_KEY = process.env.SOCIAL_MEDIA_API_KEY;
---
+++ src/models/config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_DIALECT } from '../config.js';
+
+export default {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: DB_DIALECT
+  }
+};
